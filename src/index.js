3a1b977ac84e5db7d746a1c7631ef4dfe574a02b
SBurnt <zip2004zip@gmail.com>
2020-10-25 23:59:22 +0300

    task +

--- src/index.js
+++ src/index.js
@@ -1,22 +1,3 @@
 module.exports = function reverse(n) {
-    if (n < 0) {
-        let newN = +n;
-        let resultN = "";
-
-        while (n > 0) {
-            resultN = result + (newN % 10);
-            newN = parseInt(newN / 10);
-        }
-
-        return resultN;
-    } else {
-        let result = "";
-
-        while (n > 0) {
-            result = result + (n % 10);
-            n = parseInt(n / 10);
-        }
-
-        return result;
-    }
+    return +Math.abs(n).toString().split("").reverse().join("");
 };
